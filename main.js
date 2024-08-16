1163e01f3a3e99142e8eeb0292ac2242773b178d
Alexandre Ferrreira <lexanjos@icloud.com>
2024-08-16 14:54:38 +0100

    chore: Optimize child process handling for parallel book counting

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
-// main.js
 const { fork } = require("child_process");
+const os = require("os");
+
+// Get the number of CPU cores
+const cpuCount = os.cpus().length;
+
+const optimalChildCount = Math.min(cpuCount - 2, cpuCount);
 
 const books = ["book1.txt", "book2.txt", "book3.txt"];
 const childs = [];
 
-books.forEach((book) => {
+for (let i = 0; i < optimalChildCount; i++) {
   const child = fork("counter.js");
-  child.send({ bookName: book });
+  console.log(`Forked child ${i}`);
   childs.push(child);
-});
+}
 
 childs.forEach((child, index) => {
   child.on("message", (result) => {
     console.log(`${books[index]} has ${result.count} occurrences of 'castle'`);
   });
+});
+
+books.forEach((book) => {
+  for (let i = 0; i < childs.length; i++) {
+    const child = childs[i];
+    if (child) {
+      child.send({ bookName: book });
+      break;
+    }
+  }
 });
